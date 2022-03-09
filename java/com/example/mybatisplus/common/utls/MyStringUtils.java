300843cf12b49e30be73206dc0861e5888c16204
ghost-first <63547539+ghost-first@users.noreply.github.com>
2022-03-09 15:32:17 +0800

    Delete java/com/example/mybatisplus directory

--- java/com/example/mybatisplus/common/utls/MyStringUtils.java
+++
@@ -1,9 +0,0 @@
-package com.example.mybatisplus.common.utls;
-
-import org.apache.commons.lang3.StringUtils;
-
-public class MyStringUtils {
-    public static boolean isEmptyOrIsNull(String s){
-        return StringUtils.isBlank(s);
-    }
-}
